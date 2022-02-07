53c984016e4e896231357f44c94c7f4bcd738152
Michael Westbay <westbaystars@gmail.com>
2022-02-07 16:43:02 +0900

    Added Absinthe and AbsinthePlug to dependencies.

--- mix.exs
+++ mix.exs
@@ -46,7 +46,9 @@ defmodule PlateSlate.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:absinthe, "~> 1.5"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
