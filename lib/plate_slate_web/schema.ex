60a8964fc24a2c7c71a7f9cad8ff99a3059d601e
Michael Westbay <westbaystars@gmail.com>
2022-02-08 16:39:49 +0900

    Added the price along with descriptors of the various menu item fields.

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -12,8 +12,13 @@ defmodule PlateSlateWeb.Schema do
   end
 
   object :menu_item do
+    @desc "The internal ID of a given menu item"
     field :id, :id
+    @desc "The name of the item on the menu"
     field :name, :string
+    @desc "A description of the menu item"
     field :description, :string
+    @desc "The cost of the menu item"
+    field :price, :float
   end
 end
