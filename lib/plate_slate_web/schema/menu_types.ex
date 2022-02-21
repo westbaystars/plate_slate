ca22d6206b9db71f182b66a4ead116d63f2e6359
Michael Westbay <westbaystars@gmail.com>
2022-02-21 09:02:29 +0900

    Handle failed menu item requests.

--- lib/plate_slate_web/schema/menu_types.ex
+++ lib/plate_slate_web/schema/menu_types.ex
@@ -64,4 +64,15 @@ defmodule PlateSlateWeb.Schema.MenuTypes do
     field :price, non_null(:decimal)
     field :category_id, non_null(:id)
   end
+
+  object :menu_item_result do
+    field :menu_item, :menu_item
+    field :errors, list_of(:input_error)
+  end
+
+  @desc "An error encountered trying to persist input"
+  object :input_error do
+    field :key, non_null(:string)
+    field :message, non_null(:string)
+  end
 end
--- test/plate_slate_web/schema/mutation/create_menu_item_test.exs
+++ test/plate_slate_web/schema/mutation/create_menu_item_test.exs
@@ -47,4 +47,29 @@ defmodule PlateSlateWeb.Schema.Mutation.CreateMenuTest do
         }
       }
   end
+
+  test "creating a menu item with an existing name fails", %{category_id: category_id} do
+    menu_item = %{
+      "name" => "Reuben",
+      "description" => "Roast beef, caramelized onions, horseradish, ...",
+      "price" => "5.75",
+      "categoryId" => category_id,
+    }
+    conn = build_conn()
+    conn = post conn, "/api",
+      query: @query,
+      variables: %{"menuItem" => menu_item}
+
+    assert json_response(conn, 200) == %{
+      "data" => %{"menuItem" => nil},
+      "errors" => [
+        %{
+          "locations" => [%{"column" => 3, "line" => 2}],
+          "message" => "Could not create menu item",
+          "details" => %{"name" => ["has already been taken"]},
+          "path" => ["menuItem"]
+        }
+      ]
+    }
+  end
 end
